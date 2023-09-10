645eb04f5302b5f2a4526ee9b4139301147e5d60
hefeholuwah <65517123+hefeholuwah@users.noreply.github.com>
2023-09-10 18:33:43 +0000

    made changes

--- django_project/views.py
+++ django_project/views.py
@@ -2,15 +2,15 @@ from django.http import JsonResponse
 from datetime import datetime, timedelta
 
 def get_info(request):
-    slack_name = request.GET.get('HNGx')
-    track = request.GET.get('Backend')
+    slack_name = "Hngx"
+    track = "BackendTrack"
 
     current_day = datetime.now().strftime('%A')
 
     current_time = datetime.utcnow() + timedelta(hours=2)  # Adjust for UTC+2
 
-    file_url = 'https://github.com/your_username/your_repository/blob/main/app.py'
-    source_code_url = 'https://github.com/your_username/your_repository'
+    file_url = 'https://github.com/hefeholuwah/ProudWaryProtocol/blob/main/django_project/views.py'
+    source_code_url = 'https://github.com/hefeholuwah/ProudWaryProtocol'
 
     response_data = {
         'slack_name': slack_name,
